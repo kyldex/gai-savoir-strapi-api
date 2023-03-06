24b2a2f65e527d98ebf17e3a9c42ae8efb7297ec
kyldex <raphael.giagnorio@gmail.com>
2023-03-06 23:07:27 +0100

    update plugin homepage

--- src/plugins/preview-button/admin/src/pages/HomePage/index.tsx
+++ src/plugins/preview-button/admin/src/pages/HomePage/index.tsx
@@ -7,11 +7,18 @@
 import React from 'react';
 import pluginId from '../../pluginId';
 
-const HomePage: React.VoidFunctionComponent = () => {
+const HomePage: React.FC = () => {
+  const containerStyle = {
+    display: 'flex',
+    justifyContent: 'center'
+  };
+  const titleStyle = {
+    marginTop: '15%'
+  }
+
   return (
-    <div>
-      <h1>{pluginId}&apos;s HomePage</h1>
-      <p>Happy coding</p>
+    <div style={containerStyle}>
+      <h1 style={titleStyle}>Page du plugin "{pluginId}" développé pour l'admin du Gai Savoir.</h1>
     </div>
   );
 };
