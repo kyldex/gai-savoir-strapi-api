7c40a68dde173a5ea4cf6803c027bbefde017a89
kyldex <raphael.giagnorio@gmail.com>
2024-07-10 22:48:13 +0200

    fix(preview-plugin): add category and subcategory slugs to preview url

--- src/plugins/preview-button/admin/src/components/PreviewButton/index.jsx
+++ src/plugins/preview-button/admin/src/components/PreviewButton/index.jsx
@@ -1,19 +1,68 @@
-import React from "react";
-import { useCMEditViewDataManager } from "@strapi/helper-plugin";
-import Eye from "@strapi/icons/Eye";
-import { LinkButton } from "@strapi/design-system/LinkButton";
+import React, { useEffect, useState } from 'react';
+import { useCMEditViewDataManager } from '@strapi/helper-plugin';
+import Eye from '@strapi/icons/Eye';
+import { LinkButton } from '@strapi/design-system/LinkButton';
 
 const PreviewLink = () => {
-  const { initialData } = useCMEditViewDataManager();
-  if (!initialData.slug) return null;
+  const { modifiedData } = useCMEditViewDataManager();
+  const subcategoryId = modifiedData.subcategory?.[0]?.id;
+  const [slugs, setSlugs] = useState({
+    post: modifiedData.slug,
+    category: null,
+    subcategory: null,
+  });
 
-  const previewUrl = `${process.env.STRAPI_ADMIN_CLIENT_FRONTEND_URL}/api/preview?secret=${process.env.STRAPI_ADMIN_CLIENT_PREVIEW_SECRET}&slug=${initialData.slug}`;
+  useEffect(() => {
+    const fetchSubcategoryData = async () => {
+      if (subcategoryId === undefined) return;
+
+      try {
+        const subcategoryResponse = await fetch(
+          `${process.env.STRAPI_ADMIN_CLIENT_API_URL}/subcategories/${subcategoryId}?populate=category`
+        );
+        const subcategoryData = await subcategoryResponse.json();
+        setSlugs((slugs) => ({
+          ...slugs,
+          category:
+            subcategoryData.data.attributes.category.data.attributes.slug,
+          subcategory: subcategoryData.data.attributes.slug,
+        }));
+      } catch (error) {
+        console.error('Error fetching subcategory data:', error);
+      }
+    };
+
+    fetchSubcategoryData();
+  }, [subcategoryId]);
+
+  if (!slugs.post || !slugs.category || !slugs.subcategory) {
+    return (
+      <LinkButton
+        disabled
+        size="S"
+        startIcon={<Eye />}
+        style={{ width: '100%', textDecoration: 'none' }}
+        href=""
+        variant="secondary"
+        title="page preview"
+      >
+        Missing Preview Info
+      </LinkButton>
+    );
+  }
+
+  const previewUrl =
+    `${process.env.STRAPI_ADMIN_CLIENT_FRONTEND_URL}/api/preview?` +
+    `secret=${process.env.STRAPI_ADMIN_CLIENT_PREVIEW_SECRET}` +
+    `&post-slug=${slugs.post}` +
+    `&category-slug=${slugs.category}` +
+    `&subcategory-slug=${slugs.subcategory}`;
 
   return (
     <LinkButton
       size="S"
       startIcon={<Eye />}
-      style={{ width: "100%" }}
+      style={{ width: '100%', textDecoration: 'none' }}
       href={previewUrl}
       variant="secondary"
       target="_blank"
