385cfbc8d0061e88b5396fc8e29cbe339022825a
kyldex <raphael.giagnorio@gmail.com>
2024-11-04 00:03:21 +0100

    feat(revalidate-front-end-page): add isr for audiovisual productions content types

--- src/index.ts
+++ src/index.ts
@@ -16,27 +16,27 @@ export default {
    */
   async bootstrap({ strapi }) {
     // Migration script.
-    // Add the `category` field to ideas, with a default value set to `articles`.
-    async function setDefaultCategoryForIdeas() {
+    // Add the `category` field to AV productions, with a default value set to `production-audiovisuelle`.
+    async function setDefaultCategoryForAVProductions() {
       // Check if the migration has already been executed.
       const migrationExists = await strapi.db
         .query('api::migration.migration')
         .findOne({
-          where: { name: 'setDefaultCategoryForIdeas' },
+          where: { name: 'setDefaultCategoryForAVProductions' },
         });
 
       if (migrationExists) {
-        console.log('Migration "setDefaultCategoryForIdeas" already done.');
+        console.log('Migration "setDefaultCategoryForAVProductions" already done.');
         return;
       }
 
       // Start migration
-      const ideas = await strapi.db.query('api::idea.idea').findMany();
-      for (const idea of ideas) {
-        if (!idea.category) {
-          await strapi.db.query('api::idea.idea').update({
-            where: { id: idea.id },
-            data: { category: 'articles' },
+      const audiovisualProductions = await strapi.db.query('api::audiovisual-production.audiovisual-production').findMany();
+      for (const audiovisualProduction of audiovisualProductions) {
+        if (!audiovisualProduction.category) {
+          await strapi.db.query('api::audiovisual-production.audiovisual-production').update({
+            where: { id: audiovisualProduction.id },
+            data: { category: 'production-audiovisuelle' },
           });
         }
       }
@@ -44,16 +44,16 @@ export default {
       // Add a flag to indicate that the migration has been executed
       await strapi.db.query('api::migration.migration').create({
         data: {
-          name: 'setDefaultCategoryForIdeas',
+          name: 'setDefaultCategoryForAVProductions',
           executed_at: new Date(),
         },
       });
 
       console.log(
-        'Migration done : category field added to ideas.'
+        'Migration done : category field added to audiovisual productions.'
       );
     }
 
-    await setDefaultCategoryForIdeas();
+    await setDefaultCategoryForAVProductions();
   },
 };
