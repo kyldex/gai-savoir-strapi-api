29cb78473595a457b7e08bddf135bbb2107aa416
kyldex <raphael.giagnorio@gmail.com>
2024-11-04 17:44:56 +0100

    chore: bump strapi version to 4.25.1

--- src/admin/webpack.config.js
+++ src/admin/webpack.config.js
@@ -1,12 +1,17 @@
 "use strict";
 
+// Temporary fix : DefinePlugin seems not to be part anymore of the Strapi webpack config.
+// We have to require it, instead of using below `webpack.DefinePlugin`, we use `DefinePlugin`.
+// See https://github.com/strapi/strapi/issues/20340#issuecomment-2144514250
+const { DefinePlugin } = require("webpack");
+
 /* eslint-disable no-unused-vars */
 module.exports = (config, webpack) => {
   // Note: we provide webpack above so you should not `require` it
   // Perform customizations to webpack config
   // Important: return the modified config
   config.plugins.push(
-    new webpack.DefinePlugin({
+    new DefinePlugin({
       STRAPI_ADMIN_CLIENT_FRONTEND_URL: JSON.stringify(process.env.STRAPI_ADMIN_CLIENT_FRONTEND_URL),
       STRAPI_ADMIN_CLIENT_PREVIEW_SECRET: JSON.stringify(process.env.STRAPI_ADMIN_CLIENT_PREVIEW_SECRET),
     })
